7f7085fc335ece80431c1fcad71bc8b9c6583089
VictorLiYim <victor.li-yim@efrei.net>
2024-03-28 14:53:27 +0100

    premier test

--- functions.c
+++ functions.c
@@ -1,1 +1,9 @@
 #include "functions.h"
+
+COLUMN *create_column(char* title)
+{
+    COLUMN *column;
+    column->name = title;
+    return column;
+
+}
--- functions.h
+++ functions.h
@@ -2,4 +2,15 @@
 #ifndef CDATAFRAME_LI_YIM_LADEIRA_A_FUNCTIONS_H
 #define CDATAFRAME_LI_YIM_LADEIRA_A_FUNCTIONS_H
 
+#define REALOC_SIZE 256
+
+typedef struct
+{
+    char *name;
+    int data;
+}COLUMN;
+
+
+COLUMN *create_column(char* title);
+
 #endif //CDATAFRAME_LI_YIM_LADEIRA_A_FUNCTIONS_H
