552658285196330988375983528b53998e3249f3
VictorLiYim <victor.li-yim@efrei.net>
2024-04-21 17:50:48 +0200

    opérations usuelles + analyse et statistiques 4.2.1/3 et 4)

--- cdataframe.c
+++ cdataframe.c
@@ -89,7 +89,9 @@ void print_CDataframe_limited_raw(CDataframe* cdataframe){
         printf("Column %d (%s):\n", i + 1, cdataframe->columns[i]->name);
         printf("Values:\n");
         for(j = 0; j<limit; j++){
-            printf("[%d] %d\n", j, cdataframe->columns[i]->data[j]);
+            if(cdataframe->columns[i]->data[j]!=NULL) {
+                printf("[%d] %d\n", j, cdataframe->columns[i]->data[j]);
+            }
         }
     }
 }
@@ -109,6 +111,35 @@ void print_CDataframe_limited_columns(CDataframe* cdataframe){
     }
 
     }
+
+void add_raw(CDataframe* cdataframe){
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        int val;
+        printf("Entrez la valeur à ajouter pour la colonne %s : ", cdataframe->columns[i]->name);
+        scanf("%d", &val);
+        insert_value(cdataframe->columns[i], val);
+    }
+}
+
+void delete_raw(CDataframe* cdataframe) {
+    int index;
+    do {
+        printf("Entrez l'indice de la ligne à supprimer : ");
+        scanf("%d", &index);
+    } while (index < 0);
+
+    // Supprimer la ligne de chaque colonne
+    for (int i = 0; i < cdataframe->num_columns; i++) {
+        if (index < cdataframe->columns[i]->TL) {
+            // Supprimer la valeur à l'index spécifié
+            for (int j = index; j < cdataframe->columns[i]->TL - 1; j++) {
+                cdataframe->columns[i]->data[j] = cdataframe->columns[i]->data[j + 1];
+            }
+            cdataframe->columns[i]->TL--;
+        }
+    }
+}
+
 
 void add_column(CDataframe* cdataframe) {
     if (cdataframe == NULL) {
@@ -142,4 +173,153 @@ void add_column(CDataframe* cdataframe) {
     // Ajouter la nouvelle colonne à la fin du tableau
     cdataframe->columns[cdataframe->num_columns] = column;
     cdataframe->num_columns++;
+}
+
+void delete_columncdf(CDataframe* cdataframe) {
+    int index;
+    do {
+        printf("Entrez l'indice de la colonne à supprimer : ");
+        scanf("%d", &index);
+    } while (index < 0);
+    if (cdataframe == NULL || index < 0) {
+        printf("ERREUR\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(index >= cdataframe->num_columns){
+        printf("Il n'y a pas de colonne à cet indice fin de l'exécution");
+        exit(EXIT_FAILURE);
+    }
+
+    // Libérer la mémoire allouée pour la colonne à l'index spécifié
+    delete_column(&(cdataframe->columns[index]));
+    // Décaler les colonnes restantes vers la gauche pour remplir l'espace laissé par la suppression
+    for (int i = index; i < cdataframe->num_columns - 1; i++) {
+        cdataframe->columns[i] = cdataframe->columns[i + 1];
+    }
+    // Réduire le nombre de colonnes dans le dataframe
+    cdataframe->num_columns--;
+}
+
+void rename_title(CDataframe* cdataframe){
+    int index;
+    do {
+        printf("Entrez l'indice de la colonne à modifier : ");
+        scanf("%d", &index);
+    } while (index < 0);
+    if(index >= cdataframe->num_columns){
+        printf("Il n'y a pas de colonne à cet indice fin de l'exécution");
+        exit(EXIT_FAILURE);
+    }
+    free(cdataframe->columns[index]->name);
+    char title[50];
+    printf("Entrez le nouveau titre de la colonne %d: ", index);
+    scanf("%49s", title);
+    cdataframe->columns[index]->name = strdup(title);
+    if (cdataframe->columns[index]->name == NULL) {
+        printf("ERREUR\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int verify(CDataframe* cdataframe){
+    int val;
+    printf("Saisir la valeur que vous recherchez : ");
+    scanf("%d", &val);
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        for(int j = 0; j<cdataframe->columns[i]->TL; j++){
+            if(cdataframe->columns[i]->data[j] == val){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+void search(CDataframe* cdataframe){
+    int option;
+    do {
+        printf("Que voulez-vous faire entre : \n -0 acceder\n -1 modifier \n");
+        scanf("%d", &option);
+    }while(option < 0 || option > 1);
+    int num_raw, num_col;
+    printf("Entrez l'indice de la colonne et de la ligne : ");
+    scanf("%d %d", &num_col, &num_raw);
+    printf("\n");
+    if(option == 0){
+        printf("La valeur que vous cherchez à la colonne %d ligne %d est %d\n", num_col, num_raw, cdataframe->columns[num_col]->data[num_raw]);
+    }
+    else{
+        int new_val;
+        printf("Saisissez la nouvelle valeur : ");
+        scanf("%d", &new_val);
+        cdataframe->columns[num_col]->data[num_raw]= new_val;
+        printf("Modification effectuée");
+    }
+}
+void print_name(CDataframe* cdataframe){
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        printf("Titre de la colonne %d : %s\n", i+1,cdataframe->columns[i]->name);
+    }
+}
+
+void print_num_raw(CDataframe* cdataframe){
+    if(cdataframe == NULL){
+        printf("ERREUR");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        printf("Nombre de ligne de la colonne %s : %d", cdataframe->columns[i]->name, cdataframe->columns[i]->TL);
+    }
+}
+
+void print_num_col(CDataframe* cdataframe){
+    if(cdataframe == NULL){
+        printf("ERREUR");
+        exit(EXIT_FAILURE);
+    }
+    printf("Il y a %d colonnes.", cdataframe->num_columns);
+}
+
+int research_cel_equal(CDataframe* cdataframe, int x){
+    int count, verif;
+    if(cdataframe == NULL){
+        printf("ERREUR");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        verif = research_equal(cdataframe->columns[i], x);
+        if(verif != 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+int research_cel_sup(CDataframe* cdataframe, int x){
+    int count, verif;
+    if(cdataframe == NULL){
+        printf("ERREUR");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        verif = research_sup(cdataframe->columns[i], x);
+        if(verif != 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+int research__celinf(CDataframe* cdataframe, int x){
+    int count, verif;
+    if(cdataframe == NULL){
+        printf("ERREUR");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i<cdataframe->num_columns; i++){
+        verif = research_inf(cdataframe->columns[i], x);
+        if(verif != 0){
+            count++;
+        }
+    }
+    return count;
 }
--- cdataframe.h
+++ cdataframe.h
@@ -12,12 +12,32 @@ typedef struct {
     int num_columns;   // Nombre de colonnes
 } CDataframe;
 
+//Alimentation
 CDataframe* create_CDataframe(int num_columns);
 void read_CDataframe(CDataframe* cdataframe);
-void print_CDataframe(CDataframe* cdataframe);
 void read_cdataframe_hardway(CDataframe* cdataframe);
+
+//Affichage
+void print_CDataframe(CDataframe* cdataframe);
 void print_CDataframe_limited_raw(CDataframe* cdataframe);
-void print_CDataframe_limited_columns(CDataframe* cdatafram);
+void print_CDataframe_limited_columns(CDataframe* cdataframe);
+
+//Opérations usuelles
+void add_raw(CDataframe* cdataframe);
+void delete_raw(CDataframe* cdataframe);
 void add_column(CDataframe* cdataframe);
+void delete_columncdf(CDataframe* cdataframe);
+void rename_title(CDataframe* cdataframe);
+int verify(CDataframe* cdataframe);
+void search(CDataframe* cdataframe);
+void print_name(CDataframe* cdataframe);
+
+//Analyse et statistiques
+
+void print_num_raw(CDataframe* cdataframe);
+void print_num_col(CDataframe* cdataframe);
+int research_cel_equal(CDataframe* cdataframe, int x);
+int research_cel_sup(CDataframe* cdataframe, int x);
+int research__celinf(CDataframe* cdataframe, int x);
 
 #endif //CDATAFRAME_LI_YIM_LADEIRA_A_CDATAFRAME_H
